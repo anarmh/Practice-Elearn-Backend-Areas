55b97c38de56a11dca7a4e2bd6f9919152f099e0
anarmh <anarmh@code.edu.az>
2023-05-16 21:23:01 +0400

    add Elearn template backend

---
+++ Elearn/Elearn/Models/Slider.cs
@@ -0,0 +1,10 @@
+﻿namespace Elearn.Models
+{
+    public class Slider:BaseEntity
+    {
+        public string Image { get; set; }
+        public string Logo { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
